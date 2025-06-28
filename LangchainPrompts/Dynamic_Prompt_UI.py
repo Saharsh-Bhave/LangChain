1a036fabbc224f14cc7bf28bfaa711d298820080
Your Name <you@example.com>
2025-06-27 18:03:22 -0700

    Research_Tool using Dynamic prompt

--- LangchainPrompts/Dynamic_Prompt_UI.py
+++ LangchainPrompts/Dynamic_Prompt_UI.py
@@ -2,6 +2,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
@@ -10,13 +11,33 @@ model = ChatOpenAI(model='gpt-4')
 st.header("Research Tool")
 
 paper_input = st.selectbox("Select research paper name", ["Select...", "Attention is all you need", "BERT: Pre-Training of Deep Biderictional Transformers", "GPT-3: Language Models are a Few Short Learners", "Diffusion Models Beat GANs on Image Synthesis"])
-style_input = st.selectbox("Select your style", ["Select...", "Beginner-Friendly", "Intermediate", "Expert"])
-length_input = st.selectbox("Select your length", ["Select...", "Short", "Medium", "Long"])
+style_input = st.selectbox("Select your style", ["Select...", "Beginner-Friendly", "Technical", "Code-oriented", "Mathematical"])
+length_input = st.selectbox("Select your length", ["Select...", "Short(1-2 paragraphs)", "Medium(3-4 paragraphs)", "Long(Detailed explanation)"])
 
+#template
+template = PromptTemplate(
+    template="""
+Please summarize the research paper titled "{paper_input}" with the following specifications:
+Explanation style: "{style_input}"
+Explanation Length: "{length_input}"
+1. Mathematical details:
+    - Include relevant mathematical equaitons if present in the paper.
+    - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
+2. Analogies:
+    - Use relatable analogies to simplify complex ideas.
+If certain information is not available in the paper, respond with:"Insufficient Information" instead of guessing.
+Ensure summary is clear, accurate, and aligned with the provided style and length.
+""",
+input_variables=['paper_input', 'style_input', 'length_input']
+)
 
-
-user_input = st.text_input("Enter your query.")
+#fill the placeholder
+prompt = template.invoke({
+    'paper_input':paper_input,
+    'style_input':style_input,
+    'length_input':length_input
+})
 
 if st.button("Summarizer"):
-    result = model.invoke(user_input)
+    result = model.invoke(prompt)
     st.write(result.content)
