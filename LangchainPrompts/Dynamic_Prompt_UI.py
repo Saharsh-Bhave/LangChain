b5316db8cf70321d835a36a79aa0073307abab9e
Your Name <you@example.com>
2025-06-29 17:45:21 -0700

    loading the prompt

--- LangchainPrompts/Dynamic_Prompt_UI.py
+++ LangchainPrompts/Dynamic_Prompt_UI.py
@@ -2,7 +2,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 load_dotenv()
 
@@ -14,22 +14,7 @@ paper_input = st.selectbox("Select research paper name", ["Select...", "Attentio
 style_input = st.selectbox("Select your style", ["Select...", "Beginner-Friendly", "Technical", "Code-oriented", "Mathematical"])
 length_input = st.selectbox("Select your length", ["Select...", "Short(1-2 paragraphs)", "Medium(3-4 paragraphs)", "Long(Detailed explanation)"])
 
-#template
-template = PromptTemplate(
-    template="""
-Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation style: "{style_input}"
-Explanation Length: "{length_input}"
-1. Mathematical details:
-    - Include relevant mathematical equaitons if present in the paper.
-    - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-2. Analogies:
-    - Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with:"Insufficient Information" instead of guessing.
-Ensure summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables=['paper_input', 'style_input', 'length_input']
-)
+template = load_prompt('template.json')
 
 #fill the placeholder
 prompt = template.invoke({
