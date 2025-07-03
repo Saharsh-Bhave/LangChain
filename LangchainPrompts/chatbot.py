6ddd89293a26b64bd41fa8930002c605e9cdf10e
Your Name <you@example.com>
2025-07-02 19:56:34 -0700

    ChatBot with no memory

---
+++ LangchainPrompts/chatbot.py
@@ -0,0 +1,14 @@
+#ChatBot with no memory of the previous conversation.
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+while True:
+    user_input = input("You: ")
+    if user_input == "exit" :
+        break
+    result = model.invoke(user_input)
+    print("ChatBot: ", result.content)
