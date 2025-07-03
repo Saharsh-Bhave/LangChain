f0c7a769d892a5077c21431b791ff68ba075e47c
Your Name <you@example.com>
2025-07-02 20:36:19 -0700

    messages to add memory

--- LangchainPrompts/chatbot.py
+++ LangchainPrompts/chatbot.py
@@ -1,20 +1,24 @@
 #ChatBot with no memory of the previous conversation.
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model = ChatOpenAI()
 
-conversations = []
+conversations = [
+    SystemMessage(content="You are a helpful assistant")
+
+] # adding memory using messages
 
 while True:
     user_input = input("You: ")
-    conversations.append(user_input)
+    conversations.append(HumanMessage(content = user_input))
     if user_input == "exit" :
         break
     result = model.invoke(conversations)
-    conversations.append(result.content)
+    conversations.append(AIMessage(content = result.content))
     print("ChatBot: ", result.content)
 
 print(conversations)
