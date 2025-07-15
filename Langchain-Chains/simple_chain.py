ed4bae709d4a568255ca163761280bdb643b2929
Your Name <you@example.com>
2025-07-14 17:38:24 -0700

    simple_linear_chain

---
+++ Langchain-Chains/simple_chain.py
@@ -0,0 +1,20 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template='Give 5 interesting facts about the following {topic}.',
+    input_variables=['topic']
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic': 'Hip-Hop'})
+print(result)
